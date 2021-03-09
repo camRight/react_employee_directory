affe53457105c706bfdb4e2d210b83d5a23fb019
Cameron Wright <cam.mac.wright@gmail.com>
2021-03-08 20:49:18 -0800

    bootstrap

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,7 +1,30 @@
 import axios from "axios";
 
-const BASEURL = "https://randomuser.me/api/";
 
-export default {
-    
+function App() {
+    const [state, setState] = useState([])
+    useEffect(() => {
+        const BASEURL = "https://randomuser.me/api/";
+
+        fetch(BASEURL,
+            {
+                "method": "GET",
+                "headers": {
+                    "content-type": "application/json",
+                    "accept": "application/json"
+                }
+            }) .then(
+                res => setState(res.data)
+            )
+
+    }, [])
+
+    return (
+        <>
+        {state.map( d => <div>{d}</div>)}
+        </>
+    )
+
+    console.log()
 }
+module.exports = Api;
